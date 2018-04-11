11dcabe5af3a4a28dcecabe4e7864ffa87108ec6
Fabian Schuh <Fabian@chainsquad.com>
2018-04-11 09:23:08 +0200

    More logging

--- bookiesports/cli.py
+++ bookiesports/cli.py
@@ -9,16 +9,24 @@ log = logging.getLogger(__name__)
 
 
 @main.command()
+@click.option("--approver")
+@click.option("--proposer")
 @click.pass_context
 @onlineChain
 @unlockWallet
-def sync(ctx):
+def sync(ctx, approver, proposer):
     """ Sync the Entities in BookieSports with the blockchain
     """
     w = Lookup(peerplays_instance=ctx.peerplays)
+    if proposer:
+        w.set_proposing_account(proposer)
+    if approver:
+        w.set_approving_account(approver)
 
     # Go through all sports
     for sport in w.list_sports():
+
+        log.info("Doing sport {}".format(sport["identifier"]))
 
         # Update the sport
         sport.update()
@@ -26,16 +34,20 @@ def sync(ctx):
         # Go through all event groups of the sport
         for e in sport.eventgroups:
 
+            log.info("Doing eventgroup {}".format(e["identifier"]))
+
             # Update the event group
             e.update()
 
         # Go through all the rules linked in the sport
         for r in sport.rules:
 
+            log.info("Doing rule {}".format(r["identifier"]))
+
             # Update the rule
             r.update()
 
-    w.broadcast()
+    log.info(w.broadcast())
 
 
 if __name__ == "__main__":
