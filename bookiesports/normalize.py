593171c6fdc6c96ead7261d63e12a421b5c729ed
Roshan Syed <roshan.syed.rs@gmail.com>
2020-10-23 19:11:35 +0000

    removed some logging for couch potato CLI

--- bookiesports/normalize.py
+++ bookiesports/normalize.py
@@ -105,14 +105,16 @@ class IncidentsNormalizer(object):
         try:
             toReturn = start_date <= self._string_to_date(eventgroup["finish_date"], "to") and start_date >= self._string_to_date(eventgroup["start_date"], "from")
         except TypeError:
-            log.warning("datetime offset naive, failed")
+            # log.warning("datetime offset naive, failed")
+            pass
 
         try:
             _timezone = timezone('UTC')
             start_date = _timezone.localize(start_date)
             toReturn = start_date <= self._string_to_date(eventgroup["finish_date"], "to") and start_date >= self._string_to_date(eventgroup["start_date"], "from")
-        except TypeError: 
-            log.warning("datetime offset aware, failed")
+        except TypeError:
+            # log.warning("datetime offset aware, failed")
+            pass
         return toReturn
 
     def _get_eventgroup_identifier(self,
